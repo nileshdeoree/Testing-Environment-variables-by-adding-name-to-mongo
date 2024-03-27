e579b0552e1b5b2b5cea43b696a13901a91f696b
Nilesh Deore <nileshdeore2623@gmail.com>
2024-03-27 15:40:50 +0530

    Update getnames.js

--- pages/api/getnames.js
+++ pages/api/getnames.js
@@ -3,7 +3,7 @@ import connectDb from "../../middleware/mongoose"
 
 const handler = async (req, res) => {
 
-    let names = await Name.remove()
+    let names = await Name.findById('6603ef81b650f49402051db0')
     res.json(names)
 }
 
