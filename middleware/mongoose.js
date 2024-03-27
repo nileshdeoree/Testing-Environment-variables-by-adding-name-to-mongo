d1c99c86fad3aec96af4beba6b8f44fc7d951c52
Nilesh Deore <nileshdeore2623@gmail.com>
2024-03-27 10:36:25 +0530

    Update mongoose.js

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectDb = handler => async (req, res)=>{
     if(mongoose.connections[0].readyState){
         return handler (req, res)
     }
-    await mongoose.connect(process.env.MONGO_URI)
+    await mongoose.connect("mongodb+srv://nileshdeore:1250@cluster0.qogyo0p.mongodb.net/codeswearyoutube?retryWrites=true&w=majority&appName=Cluster0")
     return handler(req, res);
 }
 
